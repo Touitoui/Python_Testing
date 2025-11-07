a13b4db56d859a68c70cd9b143e3eef501e0c63b
Mickael <mickael.gresse@gmail.com>
2025-11-07 15:25:50 +0100

    Check the number of points before checkout

--- server.py
+++ server.py
@@ -46,6 +46,10 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+
+    if placesRequired > int(club['points']):
+        flash('You do not have enough points to book this many places.')
+        return render_template('welcome.html', club=club, competitions=competitions)
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)

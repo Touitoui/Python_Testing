9085eb221b56738a7f97f4e4f40f110405899788
Mickael <mickael.gresse@gmail.com>
2025-11-07 16:16:47 +0100

    Prevent from booking past event, client side

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import json
+from datetime import datetime
 from flask import Flask,render_template,request,redirect,flash,url_for
+from datetime import datetime
 
 
 def loadClubs():
@@ -27,7 +29,7 @@ def index():
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions, now=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
 @app.route('/book/<competition>/<club>')
@@ -38,7 +40,7 @@ def book(competition,club):
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions, now=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
 @app.route('/purchasePlaces',methods=['POST'])
@@ -48,7 +50,7 @@ def purchasePlaces():
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions, now=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
 # TODO: Add route for points display

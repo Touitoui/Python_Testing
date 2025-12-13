f24e40150fb438d8f37ed82858c72c3960fc7a25
Mickael <mickael.gresse@gmail.com>
2025-12-13 09:34:40 +0100

    Added point display with simple display

--- server.py
+++ server.py
@@ -51,7 +51,9 @@ def purchasePlaces():
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
-# TODO: Add route for points display
+@app.route('/pointDisplay')
+def pointDisplay():
+    return render_template('points.html', clubs=clubs)
 
 
 @app.route('/logout')
